ba6a8946ed75c73aaccb23d9841f71bd44d2b511
FxBresson <fx-bresson@live.fr>
2017-09-14 15:58:56 +0200

    potentiometer

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const io = require('socket.io')(server);
 
 let led = null;
 let button = null;
+let potentiometer = null;
+let board = new five.Board();
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/server'));
@@ -15,10 +17,30 @@ app.get('/', function(req, res, next) {
     res.sendFile(__dirname + '/server/index.html')
 });
 
-five.Board().on('ready', function() {
+board.Board().on('ready', function() {
     console.log('Server is ready.');
 
     button = new five.Button(2)
+
+    // Listen to the web socket connection
+    potentiometer = new five.Sensor({
+        pin: "A2",
+        freq: 250
+    });
+
+    // Inject the `sensor` hardware into the Repl instance's context allows direct command line access
+    board.repl.inject({
+        pot: potentiometer
+    });
+
+    // "data" get the current reading from the potentiometer
+    potentiometer.on("data", function() {
+        console.log(this.value);
+    });
+
+    button.on("press", function() {
+        console.log( "Button pressed" );
+    });
 
     // Listen to the web socket connection
     io.on('connection', function(client) {
